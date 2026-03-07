917f3a54b838937ee11ae5927ce2107a86768387
yash-ag-online <yashag.online@gmail.com>
2026-03-07 16:36:29 +0530

    Initial Express Setup Ready

--- src/app.js
+++ src/app.js
@@ -1,6 +1,72 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import morgan from 'morgan';
+import { ApiError, ApiResponse, asyncHandler, envValues } from './utils.js';
+import { statusCodes } from './constants.js';
 
 const app = express();
+
+// Common Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(
+  morgan(`:method :url :status :res[content-length] - :response-time ms`),
+);
+app.use(
+  cors({
+    origin: envValues.origins,
+    methods: ['POST', 'GET', 'PATCH', 'OPTION', 'PUT'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  }),
+);
+
+// Routes
+app.get(
+  '/health-check',
+  asyncHandler((req, res) => {
+    return res
+      .status(statusCodes.OK)
+      .json(new ApiResponse(statusCodes.OK, 'Health Ok!', null));
+  }),
+);
+
+// Not-Found Handler
+app.use(
+  asyncHandler((req, res) => {
+    return res
+      .status(statusCodes.NOT_FOUND)
+      .json(new ApiResponse(statusCodes.NOT_FOUND, 'Route Not Found!', null));
+  }),
+);
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res
+      .status(err.statusCode)
+      .json(new ApiError(err.statusCode, err.message, err.error));
+  }
+
+  if (err instanceof Error) {
+    return res
+      .status(statusCodes.INTERNAL_SERVER_ERROR)
+      .json(new ApiError(statusCodes.INTERNAL_SERVER_ERROR, err.message, null));
+  }
+
+  return res
+    .status(statusCodes.INTERNAL_SERVER_ERROR)
+    .json(
+      new ApiError(
+        statusCodes.INTERNAL_SERVER_ERROR,
+        'Something Went Wrong!',
+        null,
+      ),
+    );
+});
 
 export default app;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,57 @@
 export const envValues = Object.freeze({
   port: Number(process.env.PORT),
+  origin: process.env.ORIGINS.toString().split(','),
+  node_env: process.env.NODE_ENV.toString(),
 });
+
+export class ApiResponse {
+  constructor(statusCode, message, data) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = statusCode < 400;
+    this.data = data;
+  }
+}
+
+export class ApiError extends Error {
+  constructor(statusCode, message, error, stack) {
+    super(message);
+
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+    this.success = false;
+    this.error = error;
+
+    Object.defineProperty(this, 'message', {
+      value: message,
+      enumerable: true,
+      writable: true,
+      configurable: true,
+    });
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+
+    Object.defineProperty(this, 'stack', {
+      value: this.stack,
+      enumerable: true,
+      writable: true,
+      configurable: true,
+    });
+
+    if (envValues.node_env !== 'development') this.stack = undefined;
+  }
+}
+
+export const asyncHandler = (handler) => {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
